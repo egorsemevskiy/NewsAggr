d93e20919d1ef537e5600c03f11491882611d600
OlegOrwell <59802673+OlegOrwell@users.noreply.github.com>
2022-06-20 02:45:18 +0300

    Update translator.py

--- translator.py
+++ translator.py
@@ -3,19 +3,15 @@ import async_google_trans_new
 
 
 class Interpreter:
-    """ For translation from string use
-    translate_from_string method with string as input
-
-    For translation from list use
-    translate_from_list method with list as input
+    """ For translation from string and list use translate() method
 
     Examples
     ----------
     some_translation = Interpreter()
-    some_translate.translate_from_string("London is the capital of Great Britain")
+    some_translate.translate("London is the capital of Great Britain")
     "Лондон - столица Великобритании"
 
-    some_translation.translate_from_list("Beatles", "Scorpions", "Blur", "Sandy goes on vacation")
+    some_translation.translate("Beatles", "Scorpions", "Blur", "Sandy goes on vacation")
     ['Битлз ', 'Скорпионы ', 'Размытие ', 'Сэнди уходит в отпуск ']
     """
 
@@ -34,3 +30,10 @@ class Interpreter:
             list_.append(self.translate_from_string(text))
         list_ = await asyncio.gather(*list_)
         return list_
+
+    def translate(self, input_):
+        loop = asyncio.get_event_loop()
+        if isinstance(input_, str) == True:
+            return loop.run_until_complete(self.translate_from_string(input_))
+        if isinstance(input_, list) == True:
+            return loop.run_until_complete(self.translate_from_list(input_))
