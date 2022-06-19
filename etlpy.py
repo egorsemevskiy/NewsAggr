ad37ab71e1e5cd5e6eb8dc7c2ddb1ab33bf9e21e
OlegOrwell <59802673+OlegOrwell@users.noreply.github.com>
2022-06-20 02:48:25 +0300

    Update etlpy.py

--- etlpy.py
+++ etlpy.py
@@ -8,22 +8,18 @@ class ETL:
     It makes translation of foreign texts,
     expands database to fit new data, and loads transformed data back to db
 
-    By default ETL addresses to db "news.db" with table name "news".
+    By default ETL addresses to db "n_db.db" with table name "news".
     You can change both names with set_db_method
 
     Use run() method to run ETL script automatically
     """
-    def __init__(self, name="news.db", table_name: str = "news",
-                 trans_from="summary", trans_to="translation"):
-        self.trans_from = trans_from
-        self.trans_to = trans_to
+    def __init__(self, name="news.db", table_name: str = "news"):
         self.name = name
         self.table_name = table_name
+        self.q = Interpreter()
         self.db = sqlite3.connect(self.name)
         self.cursor = self.db.cursor()
         self.sqlite_select_query = """SELECT * from news"""
-        self.sqlite_select_query_partial = """SELECT id, summary from news"""
-        self.dictionary = None
 
     def set_db_name(self, db_name: str = "news.db", table_name: str = "news"):
         self.name = db_name
@@ -31,26 +27,22 @@ class ETL:
 
     def update_db(self, *args):
         for arg in args[0]:
-            values = [arg['translation'], arg['id']]
-            update_query = f''' UPDATE {self.table_name}
-                          SET {self.trans_to} = ? 
+            _ = [arg['translation'], arg['id']]
+            query_ = ''' UPDATE news
+                          SET translation = ? 
                           WHERE id = ?'''
-            self.cursor.execute(update_query, values)
+            self.cursor.execute(query_, _)
             self.db.commit()
 
     def select_all(self):
         self.cursor.execute(self.sqlite_select_query)
-        #self.cursor.execute(self.sqlite_select_query_partial)
         self.records = self.cursor.fetchall()
-        self.column_names = [descript[0] for descript in self.cursor.description]
-        print("!!!", self.column_names)
-        # print(self.records)
+        print(self.records)
 
     def add_column(self, name: str = 'translation'):
-        if self.trans_to not in self.column_names:
-            sqlite_add_column = f"""ALTER TABLE news ADD COLUMN {self.trans_to} VARCHAR(200) """
-            self.cursor.execute(sqlite_add_column)
-            self.db.commit()
+        sqlite_add_column = f"""ALTER TABLE news ADD COLUMN {name} VARCHAR(200) """
+        self.cursor.execute(sqlite_add_column)
+        self.db.commit()
 
     def drop_column(self, name: str = 'translation'):
         sqlite_drop_column = f"""ALTER TABLE news DROP COLUMN {name}"""
@@ -58,37 +50,29 @@ class ETL:
         self.db.commit()
 
     def pack_dict(self):
-        body = [summary for (id_, title, link, published, summary, dump)
-                in self.records]
-        inter = Interpreter()
-        body_translate = inter.translate(body)
-        self.dictionary = [{'id': int(news_id), 'title': title, 'link': link, 'published': published, 'summary': summary,
-                            'translation': body} for (news_id, title, link, published, summary, dump), body
-                           in zip(self.records, body_translate)]
-
-    # def pack_dict(self):
-    #     body = [summary for (id, summary) in self.records]
-    #     inter = Interpreter()
-    #     body_translate = inter.translate(body)
-    #     self.dictionary = [{'id': int(id), 'summary': summary,
-    #                         'translation': body} for (id, summary), body
-    #                        in zip(self.records, body_translate)]
+        self.dicty_ = [{'id': int(id_), 'title': title, 'link': link, 'published': published, 'summary': summary,
+                        'translation': self.q.translate(summary)} for (id_, title, link, published, summary, dump)
+                       in self.records]
 
     def update(self):
-        self.update_db(self.dictionary)
+        self.update_db(self.dicty_)
 
     def print_all(self):
         self.cursor.execute(self.sqlite_select_query)
-        print_rows = self.cursor.fetchall()
-        print(*print_rows, sep="\n")
+        a = self.cursor.fetchall()
+        print(*a, sep="\n")
 
     def close(self):
         self.db.close()
 
     def run(self):
         self.select_all()
-        self.add_column()
         self.pack_dict()
         self.update()
         self.print_all()
         self.close()
+
+if __name__ == '__main__':       
+    etl = ETL()
+    #etl.add_column()
+    etl.run()
